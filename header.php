17a8bc7f019475a2fb86edc479b524bdead1c42f
YMVA <wolinxiao@foxmail.com>
2020-02-16 14:05:58 +0800

    Typecho主题initial

---
+++ header.php
@@ -0,0 +1,110 @@
+<?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="<?php $this->options->charset(); ?>" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+<?php if ($this->options->favicon): ?>
+<link rel="shortcut icon" href="<?php $this->options->favicon(); ?>" />
+<?php endif; ?>
+<title><?php $this->archiveTitle(array(
+'category'  =>  _t('分类 %s 下的文章'),
+'search'    =>  _t('包含关键字 %s 的文章'),
+'tag'       =>  _t('标签 %s 下的文章'),
+'date'      =>  _t('在 %s 发布的文章'),
+'author'    =>  _t('作者 %s 发布的文章')
+), '', ' - '); ?><?php $this->options->title(); if ($this->is('index') && $this->options->subTitle): ?> - <?php $this->options->subTitle(); endif; ?></title>
+<?php $this->header('generator=&template=&pingback=&xmlrpc=&wlw=&commentReply=&rss1=&rss2=&antiSpam=&atom='); ?>
+<link rel="stylesheet" href="<?php cjUrl('style.min.css') ?>" />
+</head>
+<body <?php if ($this->options->HeadFixed): ?>class="head-fixed"<?php endif; ?>>
+<!--[if lt IE 9]>
+<div class="browsehappy">当前网页可能 <strong>不支持</strong> 您正在使用的浏览器. 为了正常的访问, 请 <a href="https://browsehappy.com/">升级您的浏览器</a>.</div>
+<![endif]-->
+<header id="header">
+<div class="container clearfix">
+<div class="site-name">
+<?php if ($this->options->logoUrl): ?>
+<h1>
+<a id="logo" href="<?php $this->options->siteUrl(); ?>">
+<img src="<?php $this->options->logoUrl() ?>" alt="<?php $this->options->title() ?>" title="<?php $this->options->title() ?>" />
+</a>
+</h1>
+<?php else: ?>
+<h1>
+<a id="logo" href="<?php $this->options->siteUrl(); ?>"><?php if ($this->options->customTitle): $this->options->customTitle(); else: $this->options->title(); endif; ?></a>
+</h1>
+<?php endif; ?>
+</div>
+<script>function Navswith(){document.getElementById("header").classList.toggle("on")}</script>
+<button id="nav-swith" onclick="Navswith()"><span></span></button>
+<div id="nav">
+<div id="site-search">
+<form id="search" method="post" action="<?php $this->options->siteUrl(); ?>">
+<input type="text" id="s" name="s" class="text" placeholder="输入关键字搜索" required />
+<button type="submit"></button>
+</form>
+</div>
+<ul class="nav-menu">
+<li><a target="_blank" href="https://www.gxuzf.com">主页</a></li>
+<li><a href="<?php $this->options->siteUrl(); ?>">博客</a></li>
+<li><a target="_blank" href="https://blog.gxuzf.com/page/archives.html">归档</a></li>
+<li><a target="_blank" href="https://blog.gxuzf.com/page/guestbook.html">留言</a></li>
+<li><a target="_blank" href="https://blog.gxuzf.com/page/links.html">邻居</a></li>
+<?php if (!empty($this->options->Navset) && in_array('ShowCategory', $this->options->Navset)): if (in_array('AggCategory', $this->options->Navset)): ?>
+<li class="menu-parent"><a><?php if ($this->options->CategoryText): $this->options->CategoryText(); else: ?>分类<?php endif; ?></a>
+<ul>
+<?php
+endif;
+$this->widget('Widget_Metas_Category_List')->to($categorys);
+while($categorys->next()):
+if ($categorys->levels == 0):
+$children = $categorys->getAllChildren($categorys->mid);
+if (empty($children)):
+?>
+<li><a href="<?php $categorys->permalink(); ?>" title="<?php $categorys->name(); ?>"><?php $categorys->name(); ?></a></li>
+<?php else: ?>
+<li class="menu-parent">
+<a href="<?php $categorys->permalink(); ?>" title="<?php $categorys->name(); ?>"><?php $categorys->name(); ?></a>
+<ul class="menu-child">
+<?php foreach ($children as $mid) {
+$child = $categorys->getCategory($mid); ?>
+<li><a href="<?php echo $child['permalink'] ?>" title="<?php echo $child['name']; ?>"><?php echo $child['name']; ?></a></li>
+<?php } ?>
+</ul>
+</li>
+<?php
+endif;
+endif;
+endwhile;
+?>
+<?php if (in_array('AggCategory', $this->options->Navset)): ?>
+</ul>
+</li>
+<?php
+endif;
+endif;
+if (!empty($this->options->Navset) && in_array('ShowPage', $this->options->Navset)):
+if (in_array('AggPage', $this->options->Navset)):
+?>
+<li class="menu-parent"><a><?php if ($this->options->PageText): $this->options->PageText(); else: ?>其他<?php endif; ?></a>
+<ul>
+<?php
+endif;
+$this->widget('Widget_Contents_Page_List')->to($pages);
+while($pages->next()):
+?>
+<li><a href="<?php $pages->permalink(); ?>" title="<?php $pages->title(); ?>"><?php $pages->title(); ?></a></li>
+<?php endwhile;
+if (in_array('AggPage', $this->options->Navset)): ?>
+</ul>
+</li>
+<?php endif;
+endif; ?>
+</ul>
+</div>
+</div>
+</header>
+<div id="body">
+<div class="container clearfix">
---
+++ page-archives.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * 归档
+ *
+ * @package custom
+ */
+?>
+<?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
+<?php $this->need('header.php'); ?>
+<div id="main">
+<?php if (!empty($this->options->Breadcrumbs) && in_array('Pageshow', $this->options->Breadcrumbs)): ?>
+<div class="breadcrumbs">
+<a href="<?php $this->options->siteUrl(); ?>">首页</a> &raquo; <?php $this->title() ?>
+</div>
+<?php endif; ?>
+<article class="post">
+<h1 class="post-title"><a href="<?php $this->permalink() ?>"><?php $this->title() ?></a></h1>
+<?php
+$this->widget('Widget_Contents_Post_Recent', 'pageSize='.Typecho_Widget::widget('Widget_Stat')->publishedPostsNum)->to($archives);
+$year=0;
+$output = '<div id="archives">';
+while($archives->next()){
+	$year_tmp = date('Y',$archives->created);
+	if ($year > $year_tmp) {
+		$output .= '</ul>';
+	}
+	if ($year != $year_tmp) {
+		$year = $year_tmp;
+		$output .= '<h3>'.date('Y 年',$archives->created).'</h3><ul>';
+	}
+	if ($this->options->PjaxOption && $archives->hidden) {
+		$output .= '<li>'.date('m/d：',$archives->created).'<a>'. $archives->title .'</a></li>';
+	} else {
+		$output .= '<li>'.date('m/d：',$archives->created).'<a href="'.$archives->permalink .'">'. $archives->title .'</a></li>';
+	}
+}
+$output .= '</ul></div>';
+echo $output;
+?>
+</article>
+</div>
+<?php $this->need('sidebar.php'); ?>
+<?php $this->need('footer.php'); ?>
